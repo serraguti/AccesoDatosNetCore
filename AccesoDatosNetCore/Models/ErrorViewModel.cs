196f884ddedeee16b1ee78e9f4d1a3ee47789d0f
Serra <pacoserranox@gmail.com>
2021-11-15 20:26:17 +0100

    Add project files.

---
+++ AccesoDatosNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AccesoDatosNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
