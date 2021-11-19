1b40020f8d471fed0891aa02c1209af845aff9e8
Serra <pacoserranox@gmail.com>
2021-11-19 20:00:54 +0100

    commit

---
+++ AccesoDatosNetCore/Controllers/PruebaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccesoDatosNetCore.Controllers
+{
+    public class PruebaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
