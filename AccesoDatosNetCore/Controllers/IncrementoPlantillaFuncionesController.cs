b73341fbdb08de16cf3fde8253bfe125c00f12fd
Serra <pacoserranox@gmail.com>
2021-11-19 16:02:28 +0100

    commit

--- AccesoDatosNetCore/Controllers/IncrementoPlantillaFuncionesController.cs
+++ AccesoDatosNetCore/Controllers/IncrementoPlantillaFuncionesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AccesoDatosNetCore.Data;
+using AccesoDatosNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,31 @@ namespace AccesoDatosNetCore.Controllers
 {
     public class IncrementoPlantillaFuncionesController : Controller
     {
+        PlantillaContext context;
+
+        public IncrementoPlantillaFuncionesController(PlantillaContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
+            List<string> listafunciones = this.context.GetFunciones();
+            ViewBag.Funciones = listafunciones;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string funcion, int incremento)
+        {
+            List<string> listafunciones = this.context.GetFunciones();
+            ViewBag.Funciones = listafunciones;
+            //PRIMERO DEBEMOS MODIFICAR
+            int modificados = this.context.UpdatePlantillaFuncion(funcion, incremento);
+            //UNA VEZ MODIFICADOS LOS DATOS, LOS RECUPERAMOS
+            List<Plantilla> listaplantilla = this.context.GetPlantillaFunciones(funcion);
+            ViewBag.Mensaje = "Empleados modificados: " + modificados;
+            return View(listaplantilla);
         }
     }
 }
